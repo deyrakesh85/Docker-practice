82c4cf9a5f8b0b40934773ff0c5e6baeb76bb2ab
Rakesh Dey <deyrakesh85@gmail.com>
2017-12-17 01:35:31 +0530

    corrected setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [ `docker -v | echo $?` -ne 0 ]; then
         echo "\nDocker is not running/installed\n"
 else
         echo "\n`docker -v` ======>> \033[0;32m is found!!\033[0m\n"
-		docker_installed="yes"
+                docker_installed="yes"
 fi
 
 
@@ -21,24 +21,25 @@ if [ `docker-compose -v | echo $?` -ne 0 ]; then
         echo "\nDocker is not running/installed\n"
 else
         echo "\n`docker-compose -v` ======>> \033[0;32m is found!!\033[0m\n"
-		docker_compose_installed="yes"
+                docker_compose_installed="yes"
 fi
 
-if [ "$docker_installed" == "no" || "$docker_compose_installed" == "no" ]; then
 
-	echo "Exiting as pre-requisites are not found."
-	exit 1
+if [ $docker_installed = no -o $docker_compose_installed = no ]; then
+
+        echo "Exiting as pre-requisites are not found."
+        exit 1
 else
-	
-	echo "All pre-requisites are found."
-	echo "==============================="
-	echo "*****  Starting Services  *****"
-	echo "==============================="
-	sudo docker-compose up -d
-	if [ `echo $?` -e 0 ]; then
-		echo "\033[0;32mServices started successfully.\033[0m"	
-	else
-		echo "There are some problem starting the appication."
-		exit 1
-	fi
+
+        echo "All pre-requisites are found."
+        echo "==============================="
+        echo "*****  Starting Services  *****"
+        echo "==============================="
+        sudo docker-compose up -d
+        if [ `echo $?` -eq 0 ]; then
+                echo "\033[0;32mServices started successfully.\033[0m"
+        else
+                echo "There are some problem starting the appication."
+                exit 1
+        fi
 fi
